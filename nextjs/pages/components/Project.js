cc312e7bd3fef276c6a201cb40300dd20e85151e
Joaquín Cardozo <cardozojoaquinm@gmail.com>
2021-05-24 01:30:03 -0300

    avances 24-05

--- nextjs/pages/components/Project.js
+++ nextjs/pages/components/Project.js
@@ -4,92 +4,83 @@ import Slider from './Slider'
 
 const useStyles = makeStyles(theme => ({
     container: {
-        background: theme.palette.primary.dark,
-        margin: 'auto',
-        marginTop: '1rem',
+        background: theme.palette.primary.light,
         color: theme.palette.secondary.contrastText,
         height: '100%'
-    },
-    info: {
-        //padding: '0.5rem'
     },
     image: {
         display: 'flex',
         width: '100%',
         height: '100%',
     },
-    lang: {
-        margin: '0.25rem'
-    }
 }))
 export const Project = ({ flag, content }) => {
-    const classes = useStyles()
+    const styles = useStyles()
     return (
-        <Grid
-            className={classes.container}
-            container
-            direction={!flag ? "row" : "row-reverse"}
-            justify="space-between"
-            xs
-        >
+        <>
             <Grid item xs={12} sm={6}>
-                <Fade left={flag} right={!flag}>
+                <Fade
+                //left={flag}
+                //right={!flag}
+                >
                     <Slider
                         images={content.images}
-                        classes={classes}
+                        classes={styles}
                     />
                 </Fade>
             </Grid>
-            <Grid item xs={12} sm={6}>
-                <Fade left={!flag} right={flag}>
-                    <Grid
-                        container
-                        direction="column"
-                        justify="space-between"
-                        alignItems="stretch"
-                        spacing={2}
-                    >
-                        <Fade>
-                            <Grid item xs>
-                                <Typography variant="h4">
-                                    {content.title}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs>
-                                <Typography variant="h6">{content.subtitle}</Typography>
-                            </Grid>
-                            <Grid item xs>
-                                <Typography variant="body1">
-                                    {content.description}
-                                </Typography>
-                            </Grid>
+            <Grid
+                item
+                container
+                direction="column"
+                justify="space-between"
+                alignItems="stretch"
+                xs={12}
+                sm={6}
+            //spacing={2}
+            >
+                <Grid item xs>
+                    <Typography variant="h4">
+                        <Fade
+                        //left={!flag}
+                        //right={flag}
+                        >
+                            {content.title}
+                        </Fade>
+                    </Typography>
+                </Grid>
+                <Grid item xs>
+                    <Typography variant="h6">{content.subtitle}</Typography>
+                </Grid>
+                <Grid item xs>
+                    <Typography variant="body1">
+                        {content.description}
+                    </Typography>
+                </Grid>
+                <Grid
+                    item
+                    container
+                    justify="space-around"
+                    xs
+                >
+                    {
+                        content.languages.map(lang =>
                             <Grid
                                 item
                                 container
-                                justify="space-around"
+                                justify="center"
                                 xs
                             >
-                                {
-                                    content.languages.map(lang =>
-                                        <Grid
-                                            item
-                                            container
-                                            justify="center"
-                                            xs
-                                        >
-                                            <Chip
-                                                color="secondary"
-                                                label={lang}
-                                            />
-                                        </Grid>
-                                    )
-                                }
+                                <Chip
+                                    color="secondary"
+                                    label={lang}
+                                />
                             </Grid>
-                        </Fade>
-                    </Grid>
-                </Fade>
+                        )
+                    }
+                </Grid>
             </Grid>
-        </Grid >
+        </>
     )
 }
 
--- nextjs/pages/components/Projects.js
+++ nextjs/pages/components/Projects.js
@@ -1,22 +1,39 @@
-import { Grid } from '@material-ui/core'
+import { Grid, makeStyles } from '@material-ui/core'
 import { Project } from './Project'
 import { myProjects } from './Consts'
+const useStyles = makeStyles(theme => ({
+    container: {
+        background: theme.palette.primary.light,
+        color: theme.palette.secondary.contrastText,
+        height: '100%'
+    },
+    image: {
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+    },
+}))
 export const Projects = () => {
+    const styles = useStyles()
     return (
         <Grid
             container
             direction="row"
-            justify="space-between"
-            alignItems="stretch"
+            justify="center"
+            //alignItems="stretch"
             xs={12}
-            spacing={2}
+            //spacing={2}
         >
             {
                 myProjects.map((project, i) => (
                     <Grid
                         item
+                        container
+                        //md={6}
                         xs={12}
-                        md={6}
+                        //spacing={8}
+                        className={styles.container}
+                        direction={i % 2 === 1 ? "row" : "row-reverse"}
                     >
                         <Project
                             key={project.title}
@@ -26,6 +43,6 @@ export const Projects = () => {
                     </Grid>
                 ))
             }
-        </Grid>
+        </Grid >
     )
 }
